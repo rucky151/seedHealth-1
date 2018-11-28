751f7a690e036bd261c83afabb7fa044086296ea
rucky151 <rucky151@gmail.com>
2018-11-28 15:24:23 -0500

    Add colon to the warning message

--- R/altruncnorm.R
+++ R/altruncnorm.R
@@ -28,15 +28,15 @@ altruncnorm <- function(n = 100, a = 0, b = 1, mean = 0, sd = 1){
   #---------------------------------------
   #Warning messages
   if (n <= 0 | floor(n) != n) {
-    warning (paste('n must be a positive integer, your input is', n))
+    warning (paste('n must be a positive integer, your input is:', n))
   } else if (a < 0 | a > 1) {
-    warning (paste('a must be between 0 and 1, your input value is', a))
+    warning (paste('a must be between 0 and 1, your input value is:', a))
   } else if (b < 0 | b > 1) {
-    warning (paste('b must be between 0 and 1, your input value is', b))
+    warning (paste('b must be between 0 and 1, your input value is:', b))
   } else if ( mean < 0 | mean > 1){
-    warning (paste('mean must be between 0 and 1, your input value is', mean))
+    warning (paste('mean must be between 0 and 1, your input value is:', mean))
   } else if (sd < 0 | sd > 1) {
-    warning (paste('sd must be between 0 and 1, your input value is', sd))
+    warning (paste('sd must be between 0 and 1, your input value is:', sd))
   } else{j}
   #----------------------------------------
 }
